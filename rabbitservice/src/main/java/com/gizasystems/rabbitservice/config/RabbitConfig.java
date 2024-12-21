e76aecffc551fe011e29b5fa7f3006cda3b6b0b6
Saif Ad-Din <zodicman68@gmail.com>
2024-12-21 20:02:05 +0200

    feat: Added the base for the rabbit service and its configuration

---
+++ rabbitservice/src/main/java/com/gizasystems/rabbitservice/config/RabbitConfig.java
@@ -0,0 +1,28 @@
+package com.gizasystems.rabbitservice.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    public static final String QUEUE_NAME = "order_status_queue";
+    public static final String EXCHANGE_NAME = "order_exchange";
+    public static final String ROUTING_KEY = "order.status";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+}
---
+++ rabbitservice/src/test/java/com/gizasystems/rabbitservice/RabbitserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gizasystems.rabbitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
