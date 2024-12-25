76d6c8d3f645b1f355e65ee120beaa3e8640a507
Omar Kamal Shehata <omar.k.shehata@gizasystems.onmicrosoft.com>
2024-12-25 21:30:51 +0200

    Resolve servlet conflict

--- user-service/src/main/java/com/gizasystems/userservice/config/JwtAuthenticationFilter.java
+++ user-service/src/main/java/com/gizasystems/userservice/config/JwtAuthenticationFilter.java
@@ -50,6 +50,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter  {
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
             if (jwtUtil.validateToken(jwt, userDetails)) {
+
+
                 UsernamePasswordAuthenticationToken authToken =
                         new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
