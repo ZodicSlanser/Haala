c1d4a14f4d5107e36d24cc17bc5d29a742240d30
BigFish2086 <a8686.ibrahim@gmail.com>
2024-12-26 06:37:22 +0200

    feat: add OpenFeign to memic some of the Order MicroService functionalities

--- delivery-service/src/main/java/com/gizasystems/deliveryservice/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/com/gizasystems/deliveryservice/DeliveryServiceApplication.java
@@ -2,10 +2,11 @@ package com.gizasystems.deliveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.gizasystems.deliveryservice.service")
 public class DeliveryServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DeliveryServiceApplication.class, args);
 
--- delivery-service/src/main/java/com/gizasystems/deliveryservice/controller/DeliveryPersonController.java
+++ delivery-service/src/main/java/com/gizasystems/deliveryservice/controller/DeliveryPersonController.java
@@ -13,8 +13,7 @@ import com.gizasystems.deliveryservice.dto.UpdateAvailabilityRequest;
 import com.gizasystems.deliveryservice.dto.AcceptOrderRequest;
 import com.gizasystems.deliveryservice.service.DeliveryPersonService;
 
-// import org.gizasystems.orderservice.entity.OrderState;
-// import org.gizasystems.orderservice.service.OrderService;
+import com.gizasystems.deliveryservice.service.OrderService;
 
 @RestController
 @RequestMapping("/api/v1/delivery-person")
@@ -23,8 +22,8 @@ public class DeliveryPersonController {
   @Autowired
   private DeliveryPersonService deliveryPersonService;
 
-  // @Autowired
-  // private OrderService orderService;
+  @Autowired
+  private OrderService orderService;
 
   private Long getDeliveryPersonId() {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -53,33 +52,41 @@ public class DeliveryPersonController {
   //   return ResponseEntity.ok(order);
   // }
 
-  // @GetMapping("/view-waiting-orders")
-  // public ResponseEntity<List<OrderDTO>> viewWaitingOrders() {
-  //   List<OrderDTO> waitingOrders = orderService.getOrdersByState(OrderState.WAITING_FOR_DELIVERY);
-  //   return ResponseEntity.ok(waitingOrders);
-  // }
+  @GetMapping("/view-waiting-orders")
+  public ResponseEntity<List<OrderDTO>> viewWaitingOrders() {
+    List<OrderDTO> waitingOrders = orderService.getOrdersByState("WAITING_FOR_DELIVERY");
+    return ResponseEntity.ok(waitingOrders);
+  }
 
-  // @GetMapping("/view-assigned-orders")
-  // public ResponseEntity<List<OrderDTO>> viewAssignedOrders(@RequestBody ViewAssignedOrdersRequest request) {
-  //   List<OrderDTO> currentOrders = orderService.getOrdersByStateAndDeliveryID(OrderState.ON_WAY, request.getDeliveryPersonId());
-  //   return ResponseEntity.ok(currentOrders);
-  // }
+  @GetMapping("/view-assigned-orders")
+  public ResponseEntity<List<OrderDTO>> viewAssignedOrders() {
+    Long deliveryPersonId = getDeliveryPersonId();
+    if (deliveryPersonId != null) {
+      List<OrderDTO> assignedOrders = orderService.getAssignedOrders("ON_WAY", deliveryPersonId);
+      return ResponseEntity.ok(assignedOrders);
+    }
+    return ResponseEntity.badRequest().build();
+  }
 
-  // @PostMapping("/confirm-payment")
-  // public ResponseEntity<?> confirmPayment() {
-  //   orderService.updateOrderState(OrderState.DELIVERED);
-  //   return ResponseEntity.ok().build();
-  // }
+  @PostMapping("/confirm-payment/{orderId}")
+  public ResponseEntity<?> confirmPayment(@PathVariable Long orderId) {
+    OrderDTO order = orderService.updateOrderState(orderId, "DELIVERED");
+    return ResponseEntity.ok(order);
+  }
 
-  // @GetMapping("/CalculateProfit")
-  // public ResponseEntity<Double> CalculateProfit(@RequestBody CalcFeesRequest request) {
-  //   // TODO: this can be implemented using a single query to the database in the order-service
-  //   List<OrderDTO> orders = orderService.getOrdersByState(OrderState.DELIVERED, request.getDeliveryPersonId());
-  //   double profit = 0;
-  //   for (OrderDTO order : orders) {
-  //     profit += order.getDeliveryFees();
-  //   }
-  //   return ResponseEntity.ok(profit);
-  // }
+  @GetMapping("/CalculateProfit")
+  public ResponseEntity<Double> CalculateProfit() {
+    // TODO: this can be implemented using a single query to the database in the order-service
+    Long deliveryPersonId = getDeliveryPersonId();
+    if (deliveryPersonId == null) {
+      return ResponseEntity.badRequest().build();
+    }
+    List<OrderDTO> orders = orderService.getAssignedOrders("DELIVERED", deliveryPersonId);
+    double profit = 0;
+    for (OrderDTO order : orders) {
+      profit += order.getDeliveryFees();
+    }
+    return ResponseEntity.ok(profit);
+  }
 
 }
