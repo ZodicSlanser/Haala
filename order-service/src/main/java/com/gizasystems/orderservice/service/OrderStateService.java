cce38754ea3907015f69af207c12bf65a66f3085
Saif Ad-Din <zodicman68@gmail.com>
2024-12-28 18:10:40 +0200

    feat: added services to manage orders and orders' state

---
+++ order-service/src/main/java/com/gizasystems/orderservice/service/OrderStateService.java
@@ -0,0 +1,47 @@
+package com.gizasystems.orderservice.service;
+
+import com.gizasystems.orderservice.entites.Order;
+import com.gizasystems.orderservice.enums.OrderState;
+import com.gizasystems.orderservice.exceptions.InvalidOrderStateException;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class OrderStateService {
+
+
+    public void updateOrderState(Order order, OrderState newState) {
+
+        if (!isOrderStateValid(newState, order.getState()))
+            throw new InvalidOrderStateException("Invalid order state transition");
+
+        switch (newState) {
+            case PLACED -> order.setCreated_at(LocalDateTime.now());
+            case PREPARING -> order.setConfirmedAt(LocalDateTime.now());
+            case WAITING_FOR_DELIVERY -> order.setPickupAt(LocalDateTime.now());
+            case DELIVERED -> order.setDeliveredAt(LocalDateTime.now());
+            case CANCELLED -> order.setCancelledAt(LocalDateTime.now());
+            case DECLINED -> order.setDeclinedAt(LocalDateTime.now());
+            default -> {
+                throw new InvalidOrderStateException("Invalid order state");
+            }
+        }
+        order.setUpdated_at(LocalDateTime.now());
+        order.setState(newState);
+    }
+
+    boolean isOrderStateValid(OrderState newState, OrderState currentState) {
+        return switch (newState) {
+            case PLACED -> currentState == null; //only valid if the order is not placed
+            case PREPARING, CANCELLED, DECLINED ->
+                    currentState == OrderState.PLACED; //only valid if the order is placed
+            case WAITING_FOR_DELIVERY -> currentState == OrderState.PREPARING; //only valid if the order is placed
+            case DELIVERED ->
+                    currentState == OrderState.WAITING_FOR_DELIVERY; //only valid if the order is waiting for delivery
+            default -> false;
+        };
+    }
+}
+
+
