615f48bdbfc5788b661b2f36552c87339f6eec1f
Saif Ad-Din <zodicman68@gmail.com>
2025-01-02 20:57:03 +0200

    chore: removed old unneeded security filters

--- apigateway/src/main/java/com/gizasystems/apigateway/config/SecurityConfig.java
+++ apigateway/src/main/java/com/gizasystems/apigateway/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -12,20 +11,15 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
-
     private static final Logger LOG = LoggerFactory.getLogger(SecurityConfig.class);
 
     @Bean
-    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
-        http
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        LOG.info("Configuring SecurityWebFilterChain");
+        return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(auth -> auth
-                        .pathMatchers("/authenticate/**").permitAll()
-                        .pathMatchers("/admin/**").hasRole("ADMIN")
-                        .pathMatchers("/api/**").hasRole("CUSTOMER")
-                        .anyExchange().authenticated()
-                );
-        return http.build();
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .build();
     }
-
 }
